9a0f21210d819e3f443a5ba6954acf4950d6b45f
Edward Swernofsky <edabsw@gmail.com>
2014-10-15 14:15:40 -0700

    add express port binding

--- bot.js
+++ bot.js
@@ -1,11 +1,25 @@
-var   _ = require('lodash'),
+var _ = require('lodash'),
 	irc = require('irc'),
+	express = require('express'),
+	app = express(),
 
 	config = {
 		channels: ['#bestcohort'],
 		server: 'irc.foonetic.net',
 		botName: 'MathBot'
 	};
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
 
 function Bot() {
 	this.bot = new irc.Client(config.server, config.botName, {
