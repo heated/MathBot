80a48be2286cd55a5c32e6dc9ff47bad52e692bf
Edward Swernofsky <edabsw@gmail.com>
2014-10-15 17:25:32 -0700

    bot dyno

--- bot.js
+++ bot.js
@@ -1,25 +1,11 @@
 var _ = require('lodash'),
 	irc = require('irc'),
-	express = require('express'),
-	app = express(),
 
 	config = {
 		channels: ['#bestcohort'],
 		server: 'irc.foonetic.net',
 		botName: 'MathBot'
 	};
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
-
 
 function Bot() {
 	this.bot = new irc.Client(config.server, config.botName, {
@@ -37,7 +23,7 @@ You can view my source at https://github.com/heated/MathBot/blob/master/bot.js';
 	this.respondTo(/^kill mathbot$/i, function () { eval('exit'); });
 	this.respondTo(/what is love/i, "baby, don't hurt me");
 	this.respondTo(/^<3$/i, '<3');
-	this.respondTo(/^:D$/i, 'lol');
+	this.respondTo(/:D$/i, 'lol');
 }
 
 Bot.prototype = {
