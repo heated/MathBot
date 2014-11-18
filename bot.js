55ef0b58d815a1ec0dbd30aaf0a398c5e89192f9
Edward Swernofsky <edabsw@gmail.com>
2014-11-17 18:34:57 -0800

    add simple string ignoring mode, which makes mathbot sad

--- bot.js
+++ bot.js
@@ -20,15 +20,22 @@ You can view my source at https://github.com/heated/MathBot/blob/master/bot.js';
 	this.bot.addListener('message', this.respond.bind(this));
 	this.respondTo(/^mathbot( help)?$/i, helpText);
 	this.respondTo(/^man mathbot$/i, helpText);
-	this.respondTo(/^kill mathbot$/i, function () { eval('exit'); });
+	this.respondTo(/^(kill|die|go away) mathbot$/i, this.die);
+	this.respondTo(/^mathbot (kill|die|go away)$/i, this.die);
 	this.respondTo(/what is love/i, "baby, don't hurt me");
 	this.respondTo(/^<3$/i, '<3');
 	this.respondTo(/:D$/i, 'lol');
 	this.respondTo(/^mathbot say (.+)$/i, function (text) { return text.match(/^mathbot say (.+)$/i)[1]; });
+	this.respondTo(/^mathbot (shut (up|it)|cool (it|your jets)|calm (down|(your|thy)self)|(be )?quiet|(settle|tone it) down|st(op|ahp)|shh|chill)/i, this.toneItDown.bind(this));
+	this.respondTo(/mathbot (wake up|bring (it|sexy) back|two hops|sup|yo|hey|what up)/i, this.bringItBack.bind(this));
 }
 
 Bot.prototype = {
 	responses: [],
+
+	say: function (str) {
+	  this.bot.say(config.channels[0], str);
+	},
 
 	respond: function (from, to, text, message) {
 		if (from.match(/bot/i)) return;
@@ -39,12 +46,14 @@ Bot.prototype = {
 
 		if (match) {
 			var response = match.response;
-			this.bot.say(to, (typeof(response) === "string") ? response : response(text) );
+			this.say( (typeof(response) === "string") ? response : response(text) );
 		}
 
 		try {
 			var result = eval(text);
-			this.bot.say(to, (typeof(result) === "boolean" ? '!!' : '') + JSON.stringify(result));
+			if (this.canSpeak()) {
+				this.say( (typeof(result) === "boolean" ? '!!' : '') + JSON.stringify(result) );
+			}
 		} catch(e) {}
 	},
 
@@ -60,6 +69,25 @@ Bot.prototype = {
 		}
 
 		return "created!";
+	},
+
+	die: function () {
+		eval('exit');
+	},
+
+	toneItDown: function () {
+		this.chilling = setTimeout(this.bringItBack.bind(this), 300000);
+		this.say(':(');
+	},
+
+	bringItBack: function () {
+		clearTimeout(this.chilling);
+		this.chilling = false;
+		this.say(':)');
+	},
+
+	canSpeak: function () {
+		return !this.chilling || !text.match(/^('|")[^"]*\1$/);
 	}
 }
 
